668e1fa90e0ef1900e0a7b2012faee864c6c58e8
Psimon8 <121551167+Psimon8@users.noreply.github.com>
2024-11-27 13:46:36 +0000

    7.31

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ st.write(df_xml.head(20))
 if sitemap_url and url:
     try:
         sitemap_urls = extract_sitemap_urls(sitemap_root)
-        xml_urls = df_xml['g:link'].tolist()  # Assurez-vous que la colonne 'g:link' contient les URLs des produits dans le flux XML
+        xml_urls = df_xml['link'].tolist()  # Assurez-vous que la colonne 'g:link' contient les URLs des produits dans le flux XML
 
         # Trouver les URLs des produits qui ne sont pas disponibles dans le flux XML
         missing_urls = [url for url in sitemap_urls if url not in xml_urls]
